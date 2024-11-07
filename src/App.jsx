ef2623ffded73fac704c11b531c0c697b7313c1e
pedrocruz <JayJMarquez05@gmail.com>
2024-11-06 19:51:50 -0600

    Imported the navbar onto the App.jsx | Also importing our Routes, Route, and useState since I will be using those functions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-// src/App.jsx
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return <h1>Hello world!</h1>;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,3 +11,5 @@ const NavBar = () => {
     </nav>
   );
 };
+
+export default NavBar;
