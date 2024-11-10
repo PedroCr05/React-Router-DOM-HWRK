5488ee1d3262f4712a8a8d4a9c64f1c198451c30
pedrocruz <JayJMarquez05@gmail.com>
2024-11-10 10:39:19 -0600

    Hmm, not sure what's going on. Might be the fact that I haven't done the mapping yet.

--- src/components/MailboxDetails.jsx
+++ src/components/MailboxDetails.jsx
@@ -1,11 +1,12 @@
 import { useParams } from "react-router-dom";
 
 const MailboxDetails = ({ mailboxes }) => {
-  const { mailboxId } = useParams;
+  const { mailboxId } = useParams();
 
   const selectedMailbox = mailboxes.find(
     (mail) => mail._id === Number(mailboxId)
   );
+
   return (
     <>
       {/* <h2>{selectedMailbox.boxHolder}</h2> */}
