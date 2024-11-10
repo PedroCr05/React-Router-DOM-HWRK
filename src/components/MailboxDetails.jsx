9ac170fe1a6e33975396b50c01a01840fa1bdb7a
pedrocruz <JayJMarquez05@gmail.com>
2024-11-10 10:33:09 -0600

    Okay! Note to self. Do not put Parathesis at the end of useParams since that actually breaks the decontructed varaible... Oops

--- src/components/MailboxDetails.jsx
+++ src/components/MailboxDetails.jsx
@@ -1,12 +1,11 @@
 import { useParams } from "react-router-dom";
 
 const MailboxDetails = ({ mailboxes }) => {
-  const { mailboxId } = useParams();
+  const { mailboxId } = useParams;
 
   const selectedMailbox = mailboxes.find(
     (mail) => mail._id === Number(mailboxId)
   );
-  console.log(selectedMailbox);
   return (
     <>
       {/* <h2>{selectedMailbox.boxHolder}</h2> */}
