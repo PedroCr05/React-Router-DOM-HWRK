61c9df028444e8c48f54e005b83225be9d17d021
pedrocruz <JayJMarquez05@gmail.com>
2024-11-10 09:33:29 -0600

    Just testing if my router did work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import MailboxForm from "./components/MailboxForm";
 
 const App = () => {
   return (
     <>
       <NavBar />
       <h1>Post Office</h1>
+      <MailboxForm />
     </>
   );
 };
