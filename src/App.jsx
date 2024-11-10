6ca3c5e034aad45d0779d60562d1143011372848
pedrocruz <JayJMarquez05@gmail.com>
2024-11-10 09:36:41 -0600

    Removed the test import for the MailboxForm. Created my useState for selected while tagging along the useEffect. No purpose yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import MailboxForm from "./components/MailboxForm";
 
 const App = () => {
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => {
+    const getSelected = () => {};
+    getSelected(setSelected);
+  });
+
   return (
     <>
       <NavBar />
       <h1>Post Office</h1>
-      <MailboxForm />
     </>
   );
 };
