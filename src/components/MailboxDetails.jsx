2563d4fa29e9a0e07b7b99a21ed15a7b16f5471f
pedrocruz <JayJMarquez05@gmail.com>
2024-11-10 10:30:18 -0600

    So selectedMaibox is getting undefined. Interesting...

--- src/components/MailboxDetails.jsx
+++ src/components/MailboxDetails.jsx
@@ -1,19 +1,18 @@
 import { useParams } from "react-router-dom";
 
 const MailboxDetails = ({ mailboxes }) => {
-  console.log(mailboxes);
-
   const { mailboxId } = useParams();
 
   const selectedMailbox = mailboxes.find(
     (mail) => mail._id === Number(mailboxId)
   );
+  console.log(selectedMailbox);
   return (
     <>
-      <h2>{selectedMailbox.boxHolder}</h2>
+      {/* <h2>{selectedMailbox.boxHolder}</h2> */}
       <dl>
         <dt>Box Size:</dt>
-        <dd>{selectedMailbox.boxSize}</dd>
+        {/* <dd>{selectedMailbox.boxSize}</dd> */}
       </dl>
     </>
   );
