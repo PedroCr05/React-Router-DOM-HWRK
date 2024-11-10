11a4fcd75b6fc2419ecd7c5d81aeb59e225e733f
pedrocruz <JayJMarquez05@gmail.com>
2024-11-10 10:18:22 -0600

    Okay so ran a test with creatubg a function called initStateTest. Basically a premade array of objects and their tags. It works now! Just clearing having to defind somethigns but no more errors.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ import MailboxForm from "./components/MailboxForm";
 import MailboxList from "./components/MailboxList";
 import NavBar from "./components/NavBar";
 
+const initStateTest = [
+  { _id: 1, boxholder: Jay, boxSize: medium },
+  { _id: 2, boxholder: Shonnairn, boxSize: large },
+  { _id: 3, boxholder: Miyuki, boxSize: medium },
+  { _id: 4, boxholder: Juno, boxSize: small },
+];
+
 const App = () => {
-  const [mailboxes, setMailboxes] = useState({});
-  const [selected, setSelected] = useState({});
+  const [mailboxes, setMailboxes] = useState(initStateTest);
+  // const [selected, setSelected] = useState({});
 
   const addBox = (newMailboxData) => {
     newMailboxData._id = mailboxes.length + 1;
