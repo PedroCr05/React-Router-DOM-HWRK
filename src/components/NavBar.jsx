f7b7ae66e682c29e717b19ccb1ca904d6c651dc9
pedrocruz <JayJMarquez05@gmail.com>
2024-11-11 16:01:39 -0600

    Removing comments over at the Navbar.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,15 +6,12 @@ const NavBar = () => {
       <ul>
         <li>
           <Link to="/">Home</Link>
-          {/* Just the landing page for our site */}
         </li>
         <li>
           <Link to="/mailboxes">Mailboxes</Link>
-          {/* List of Mailboxes */}
         </li>
         <li>
           <Link to="/mailboxes/new">New Mailbox</Link>{" "}
-          {/* The form for mailboxes */}
         </li>
       </ul>
     </nav>
